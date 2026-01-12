bf53d20d7a99ff3fc013de3cbf024117ea3345f9
dportu <126913208+dportu@users.noreply.github.com>
2026-01-12 22:00:43 +0100

    Alineamiento de adn cerca pero no

--- Examen_DA_12_6_Dinamica/main.cpp
+++ Examen_DA_12_6_Dinamica/main.cpp
@@ -1,16 +1,35 @@
+
+/*@ <answer>
+ *
+ * Nombre y Apellidos:
+ *
+ *@ </answer> */
+
 #include <iostream>
 #include <fstream>
 #include "Matriz.h"
 using namespace std;
 
-#define COLUMNS 5
+/*@ <answer>
+  
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+ 
+ @ </answer> */
+
+
+// ================================================================
+// Escribe el código completo de tu solución aquí debajo
+// ================================================================
+//@ <answer>
+
+#define COLS 5
 #define ROWS 5
 
-//enum tAdn {A, C, G, T, MINUS};
-
-int adnToInt(char c) {
-   int resul = 0;
-   switch(c) {
+int chartoint(const char &c) {
+   int resul = -1;
+   switch(c){
       case 'A':
          resul = 0;
          break;
@@ -27,57 +46,64 @@ int adnToInt(char c) {
          resul = 4;
          break;
    }
+   return resul;
+}
+
+int recursion(const Matriz<int> &m, const Matriz<int> &v, const string &s1, const string &s2, int i, int j) {
+   
+   //caso base
+   if(i == m.numfils() && j == m.numcols()) {
+      return 0;
+   }
+   else if(i == m.numfils()-1 ) {
+
+      int res = v[chartoint(s1[i])][chartoint(s2[j])];
+      j++;
+      for(;j < m.numcols();j++) {
+         res += v[4][chartoint(s2[j])];
+      }
+      return res;
+   }
+   else if(j == m.numcols()-1) {
+
+      int res = v[chartoint(s1[i])][chartoint(s2[j])];
+      i++;
+      for(;i < m.numfils();i++) {
+         res += v[chartoint(s1[i])][4];
+      }
+      return res;
+   }
+   
+   //caso recursivo
+   int current = v[chartoint(s1[i])][chartoint(s2[j])] + recursion(m,v,s1,s2, i+1, j+1);
+   int right= recursion(m,v,s1,s2, i, j+1) + v[4][chartoint(s2[j])];
+   int down = recursion(m,v,s1,s2, i+1, j)+ v[chartoint(s1[i])][4];
+   int resul= max( current, max(right,down) );
 
    return resul;
 }
 
 void resuelveCaso() {
-   Matriz<int> valores(ROWS,COLUMNS,0);
-   
+   Matriz<int> V(ROWS, COLS, 0);
    for(int i = 0; i < ROWS; i++) {
-      for(int j = 0; j < COLUMNS; j++) {
-         int valor;
-         cin >> valor;
-         valores[i][j] = valor;
+      for(int j = 0; j < COLS; j++) {
+         int a;
+         cin >> a;
+         V[i][j] = a;
       }
    }
-
-   int numPares;
-   cin >> numPares;
-
-
-   string s1, s2;
-   for(int k = 0; k < numPares; k++) {
+   // matriz de valores inicializada
+   int parejas;
+   cin >> parejas;
+   for(int pareja = 0; pareja < parejas; pareja++) {
+      string s1, s2;
       cin >> s1 >> s2;
+      Matriz<int> m(s1.size(),s2.size(), 0);
 
-
-      //creacion y recorrido de la matriz de soluciones parciales
-      Matriz<int> m(s1.size()+1, s2.size()+1, 0);
-      for(int i = 0; i < s1.size(); i++) {
-         for(int j = 0; j < s2.size(); j++) {
-            if(i == 0 && j == 0) {
-               m[i][j] = 0;
-            }
-            else if(i == 0 || j == 0) {
-               m[i][j] = -1*i - 1*j; // deberia poner -i en la primera fila y -j en la primera columna
-            }
-            else {
-               // algoritmo
-               int up = m[i-1][j];
-               int left = m[i][j-1];
-               int value = valores[adnToInt(s1[i])][adnToInt(s2[j])];
-               int up_left = m[i-1][j-1];
-               int puntos = max(left + up, value + up_left);
-               m[i][j] = puntos;
-            }
-         }
-      }
-
-      cout << m[s1.size()][s2.size()] << "\n";
+      int resul = recursion(m, V, s1, s2, 0, 0);
+      cout << resul << "\n";
    }
    
-
-
    // leer los datos de la entrada
    
    // resolver el caso posiblemente llamando a otras funciones
